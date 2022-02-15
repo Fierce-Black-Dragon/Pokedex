562a19adbe41ab93feca33d2ce5bea2d1c00c077
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2022-02-15 23:25:33 +0530

    making api call and show result on web

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [AllFetchedPokemons, setAllFetchedPokemons] = useState([]);
+  const getAllPokemon = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=100")
+      .then((response) => response.json())
+      .then((allPokemon) => setAllFetchedPokemons(allPokemon.results));
+  };
+
+  useEffect(() => {
+    getAllPokemon();
+  }, []);
+  console.log(AllFetchedPokemons);
   return (
-    <div class="bg-gray-400">
-      <h1>ftyfyft</h1>
+    <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4 g gap-2 ">
+      {AllFetchedPokemons?.map((pokemon) => (
+        <div
+          className="bg-gray-400 w-32 m-4 rounded-lg px-5 py-7 ring-1 ring-slate-900/5 shadow-xl"
+          key={pokemon.url}
+        >
+          <p>{pokemon.name}</p>
+          <a href={pokemon.url}> more info</a>
+        </div>
+      ))}
     </div>
   );
 }
