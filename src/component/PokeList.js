81daae30b9beea9d6a50d0209270f2a5c923a008
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2022-03-04 21:08:07 +0530

    added style for pokemon and pokemon glist

--- src/component/PokeList.js
+++ src/component/PokeList.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Pokemon } from "./Pokemon";
-
+import "../style/pokeList.css";
 export const PokeList = ({ pokemons }) => {
   const handleClick = () => {};
   return (
-    <div>
+    <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4 g gap-2 ">
       {pokemons?.map((pokemon, index) => (
         <Pokemon
           pokemon={pokemon?.name}
--- src/component/Pokemon.js
+++ src/component/Pokemon.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import "../style/pokemon.css";
 export const Pokemon = ({ pokemon, handleClick }) => {
   // "https://pokeapi.co/api/v2/pokemon?limit=100";
   return (
-    <div className="pokemon">
-      <button onClick={handleClick}>
-        <strong>{pokemon}</strong>
+    <div>
+      <button className="pokemon" onClick={handleClick}>
+        {pokemon}
       </button>
     </div>
   );
